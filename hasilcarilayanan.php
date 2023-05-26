1d224f7a7a1365de36c25796650724be708aeee2
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2023-05-26 17:06:13 +0700

    Create hasilcarilayanan.php

---
+++ hasilcarilayanan.php
@@ -0,0 +1,25 @@
+<?php $kodelayanan=$_GET['kCari'];
+$kon=mysqli_connect("localhost","root","","simtvkabel");
+$sql="select * from layanan where kodelayanan like '%".$kodelayanan."%'";
+$q=mysqli_query($kon,$sql);
+$r=mysqli_query($q);
+?>
+<table>
+  <thead>
+    <tr>
+      <th>Kode Layanan</th>
+      <th>Jenis Layanan</th>
+      <th>Aksi Rekord</th>
+    </tr>
+  </thead>
+  <tbody><?php do { ?>
+    <tr>
+      <td><?php echo $r['kodelayanan'];?></td>
+      <td><?php echo $r['jenislayanan'];?></td>
+      <td>
+        <a href="koreksilayanan.php?kodelayanan=<?php echo $r['kodelayanan'];?>">Koreksi</a>
+        <a href="hapuslayanan.php?kodelayanan=<?php echo $r['kodelayanan'];?>" onclick="return confirm('Apakah yakin akan dihapus ?');">Hapus</a>
+      </td>
+    </tr><?php } while ($r=mysqli_query($q)); ?>
+  </tbody>
+</table>
